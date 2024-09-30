afd38afbca733885d49c8934ccfa365afbbdc1e3
techycereal <alexjmilliken@outlook.com>
2024-09-30 15:08:30 -0500

    test

--- app/page.tsx
+++ app/page.tsx
@@ -73,7 +73,7 @@ const Home: React.FC = () => {
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {Object.keys(services).map((key) => (
               <ServiceCard
-                key={key}
+                key={key} 
                 title={services[key].name}
                 image={services[key].image}
               />
